c58521f412bbc3b14179aecb20a86cd57fe284b4
Darrin Allred <darrin.allred@selecthealth.org>
2025-03-10 17:19:36 -0600

    feat: working multi-email in-memory cache

--- Program.cs
+++ Program.cs
@@ -1,31 +1,89 @@
-using Azure.Core;
-using Azure.Identity;
+// using Azure.Core;
+// using Azure.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
 using ExchangeTest.Extensions;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Graph;
-using Microsoft.Identity.Client;
+// using Microsoft.Identity.Client;
 
 // using Microsoft.Graph.Models;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.TokenCacheProviders.Distributed;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.Kiota.Abstractions.Authentication;
 using Microsoft.OpenApi.Models;
 using MM.Common.Api.Utilities;
-// using WebApplication = Microsoft.AspNetCore.Builder.WebApplication;
 
 var builder = WebApplication.CreateBuilder(args);
 // Logging
 builder.ConfigureElkLogging();
-NLog.LogManager.GetCurrentClassLogger().Info("Initializing...");
+var nlogger = NLog.LogManager.GetCurrentClassLogger();
+nlogger.Info("Initializing...");
+
+nlogger.Info($"Current Directory: {Environment.CurrentDirectory}");
 
 // Configuration
-IConfigurationRoot configRoot = new ConfigurationBuilder()
+var configRoot = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json")
     .AddEnvironmentVariables()
+    .AddEnvFile()
     .Build();
-builder.Configuration.AddEnvFile();
+
+var instance = configRoot["AzureAd:Instance"];
+if (instance == null)
+{
+    nlogger.Error("AzureAd:Instance is not configured");
+    throw new Exception("AzureAd:Instance is not configured");
+}
+var tenantId = configRoot["AzureAd:TenantId"];
+if (tenantId == null)
+{
+    nlogger.Error("AzureAd:TenantId is not configured");
+    throw new Exception("AzureAd:TenantId is not configured");
+}
+var clientId = configRoot["AzureAd:ClientId"];
+if (clientId == null)
+{
+    nlogger.Error("AzureAd:ClientId is not configured");
+    throw new Exception("AzureAd:ClientId is not configured");
+}
+var clientSecret = configRoot["AzureAd:ClientSecret"];
+if (clientSecret == null)
+{
+    nlogger.Error("AzureAd:ClientSecret is not configured");
+    throw new Exception("AzureAd:ClientSecret is not configured");
+}
+var redirectUri = configRoot["AzureAd:RedirectUri"];
+if (redirectUri == null)
+{
+    nlogger.Error("AzureAd:RedirectUri is not configured");
+    throw new Exception("AzureAd:RedirectUri is not configured");
+}
+var scope = configRoot["AzureAd:Scope"];
+if (scope == null)
+{
+    nlogger.Error("AzureAd:Scope is not configured");
+    throw new Exception("AzureAd:Scope is not configured");
+}
+// var email = configRoot.GetSection("Exchange")["Email"];
+// if (email == null)
+// {
+//     nlogger.Error("Exchange:Email is not configured");
+//     throw new Exception("Exchange:Email is not configured");
+// }
+// var password = configRoot.GetSection("Exchange")["Password"];
+// if (scope == null)
+// {
+//     nlogger.Error("Exchange:Password is not configured");
+//     throw new Exception("Exchange:Password is not configured");
+// }
 
 // Add services to the container.
+builder.Services.AddCors();
+
 // builder.ConfigureSwagger(configRoot);
 
 // Configure Swagger to use OAuth2
@@ -41,11 +99,11 @@ builder.Services.AddSwaggerGen(c =>
         {
             AuthorizationCode = new OpenApiOAuthFlow
             {
-                AuthorizationUrl = new Uri($"{builder.Configuration["AzureAd:Instance"]}{builder.Configuration["AzureAd:TenantId"]}/oauth2/v2.0/authorize"),
-                TokenUrl = new Uri($"{builder.Configuration["AzureAd:Instance"]}{builder.Configuration["AzureAd:TenantId"]}/oauth2/v2.0/token"),
+                AuthorizationUrl = new Uri($"{instance}/{tenantId}/oauth2/v2.0/authorize"),
+                TokenUrl = new Uri($"{instance}/{tenantId}/oauth2/v2.0/token"),
                 Scopes = new Dictionary<string, string>
                 {
-                    { "https://graph.microsoft.com/Mail.Read", "Read user email" }
+                    { scope, "Read user email" }
                 }
             }
         }
@@ -63,29 +121,40 @@ builder.Services.AddSwaggerGen(c =>
                     Id = "oauth2"
                 }
             },
-            new[] { "https://graph.microsoft.com/Mail.Read" }
+            new[] { scope }
         }
     });
 });
 
-builder.Services.AddControllers();
+// builder.Services.AddControllers();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"))
     .EnableTokenAcquisitionToCallDownstreamApi()
     .AddDistributedTokenCaches();
+
+// distributed token cache
 builder.Services.Configure<MsalDistributedTokenCacheAdapterOptions>(options =>
 {
     options.Encrypt = true;
     options.SlidingExpiration = TimeSpan.FromDays(14);
     options.DisableL1Cache = false;
 });
-builder.Services.AddDistributedSqlServerCache(options =>
-{
-    options.ConnectionString = builder.Configuration.GetConnectionString("TokenCache");
-    options.SchemaName = configRoot.GetSection("TokenCache")["SchemaName"];
-    options.TableName = configRoot.GetSection("TokenCache")["TableName"];
-});
+// 1. use SQL Server distributed token cache
+// builder.Services.AddDistributedSqlServerCache(options =>
+// {
+//     options.ConnectionString = builder.Configuration.GetConnectionString("TokenCache");
+//     options.SchemaName = configRoot.GetSection("TokenCache")["SchemaName"];
+//     options.TableName = configRoot.GetSection("TokenCache")["TableName"];
+// });
+// 2. use in memory distributed token cache
+builder.Services.AddDistributedMemoryCache();
+// 3. use simple memory cache
+builder.Services.AddMemoryCache();
+// 4. Simple scoped variable cache
+// var accessToken = "";
+// var refreshToken = "";
 
+builder.Services.AddEndpointsApiExplorer();
 
 var app = builder.Build();
 
@@ -103,115 +172,173 @@ app.UseSwaggerUI(c =>
     c.OAuthClientId(builder.Configuration["AzureAd:ClientId"]);
     c.OAuthClientSecret(builder.Configuration["AzureAd:ClientSecret"]);
     // c.OAuthUsePkce(); // Use PKCE (Proof Key for Code Exchange)
-    c.OAuth2RedirectUrl("http://localhost:5000"); // Specify your redirect URI here
+    c.OAuth2RedirectUrl(builder.Configuration["AzureAd:RedirectUri"]); // Specify your redirect URI here
 });
-app.UseHttpsRedirection();
+// app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
-app.MapControllers();
+// app.MapControllers();
 
-// app.MapGet("/", (HttpContext context, ILogger<Program> logger) =>
-// {
-//     logger.LogInformation("/{QueryString}", context.Request.QueryString);
-
-//     context.Response.Redirect("/attachments" + context.Request.QueryString);
-// });
-
-app.MapGet("/", async (HttpContext context, IConfiguration configuration, ILogger<Program> logger) =>
+app.MapGet("/api/token", ([FromServices]ILogger<Program> logger) =>
 {
-    logger.LogInformation("Query String: {QueryString}", context.Request.QueryString);
-
-    var tenantId = configuration.GetSection("AzureAd")["TenantId"];
-    if (tenantId == null)
-    {
-        logger.LogError("AzureAd:TenantId is not configured");
-        // return Results.InternalServerError("AzureAd:TenantId is not configured");
-    }
-    var clientId = configuration.GetSection("AzureAd")["ClientId"];
-    if (clientId == null)
-    {
-        logger.LogError("AzureAd:ClientId is not configured");
-        // return Results.InternalServerError("AzureAd:ClientId is not configured");
-    }
-    var clientSecret = configuration.GetSection("AzureAd")["ClientSecret"];
-    if (clientSecret == null)
-    {
-        logger.LogError("AzureAd:ClientSecret is not configured");
-        // return Results.InternalServerError("AzureAd:ClientSecret is not configured");
-    }
-    // use locally logged on user credential (az login)
-    //   - user account must have tenant wide permissions to read email
-    // var credential = new DefaultAzureCredential();
-    
-    // use client secret credential approach
-    //   - client must have tenant wide permission to read email
-    // var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
-
-    // use username password credential approach (specify user single email and password)
-    //   - this doesn't work with federated accounts (PingID)
-    // var email = configuration.GetSection("Exchange")["Email"];
-    // var password = configuration.GetSection("Exchange")["Password"];
-
-    // var credential = new UsernamePasswordCredential(email, password, tenantId, clientId);
-
-    // use interactive browser credential approach
+    // 1. use InteractiveBrowserCredential helper class
     //   - redirects browser to login.microsoftonline.com
-    // if (string.IsNullOrEmpty(context.Request.QueryString.Value))
+    // var credential = new InteractiveBrowserCredential(new InteractiveBrowserCredentialOptions
     // {
-    //     new InteractiveBrowserCredential(new InteractiveBrowserCredentialOptions
-    //     {
-    //         TenantId = tenantId,
-    //         ClientId = clientId,
-    //         RedirectUri = new Uri("http://localhost:5000")
-    //     });
-    //     return Results.Ok();
-    // }
+    //     TenantId = tenantId,
+    //     ClientId = clientId,
+    //     RedirectUri = new Uri(redirectUri)
+    // });
+    // return Results.Ok();
+
+    // 2. redirect using raw OAuth2 authorization code flow URL
+    var url = $"{instance}/{tenantId}/oauth2/v2.0/authorize?client_id={clientId}&response_type=code&redirect_uri={redirectUri}&response_mode=query&scope={scope}&state=12345";
+    logger.LogShInfo($"Redirecting to: {url}");
+    return Results.Redirect(url);
+})
+.WithName("GetToken")
+.WithDescription("Redirect to OAuth2 authorization code flow URL")
+.WithDisplayName("Get Token")
+.WithTags("Token");
+
+app.MapGet("/", async (HttpContext context, [FromServices]IMemoryCache memoryCache, [FromServices]ILogger<Program> logger) =>
+{
+    logger.LogShInfo($"Query string: {context.Request.QueryString}");
 
     var code = context.Request.Query["code"].FirstOrDefault();
+    if (string.IsNullOrEmpty(code))
+    {
+        logger.LogShError("Authorization code is missing");
+        return Results.StatusCode(StatusCodes.Status400BadRequest);
+    }
 
-    // use AuthorizationCodeCredential approach
+    // 1. use AuthorizationCodeCredential approach
     // var options = new AuthorizationCodeCredentialOptions
     // {
-    //     RedirectUri = new Uri("http://localhost:5000")
+    //     RedirectUri = new Uri(redirectUri)
     // };
     // var credential = new AuthorizationCodeCredential(tenantId, clientId, clientSecret, code, options);
     
-    // request a token and log it
-    //   - credential (i.e. authorization code) can only be used once
+    //   - either request a token and log it.  credential (i.e. authorization code) can only be used once
     //     once GetTokenAsync is call, credential cannot be use to create GraphServiceClient
- 
-    // ether this
-    // var tokenRequestContext = new TokenRequestContext(["https://graph.microsoft.com/Mail.Read"]);
+    // var tokenRequestContext = new TokenRequestContext([scope]);
     // var token = await credential.GetTokenAsync(tokenRequestContext);
-    // logger.LogInformation($"JWT Token: {token.Token}");
+    // logger.LogShInfo($"JWT Token: {token.Token}");
     // return Results.Ok(token.Token);
 
-    // or this
-    // var graphClient = new GraphServiceClient(credential);
+    //   - or save access token to in memory cache
+    // memoryCache.Set("access_token", result.AccessToken, TimeSpan.FromMinutes(60));
 
-    // use MSAL ConfidentialClientApplication approach
+    // 2. use MSAL ConfidentialClientApplication approach
     //   - retrieving refresh token is not permitted due to security reasons
     //     https://stackoverflow.com/questions/61058614/how-to-get-refresh-token-in-msal-net-c-sharp
-    var app = ConfidentialClientApplicationBuilder.Create(clientId)
-        .WithClientSecret(clientSecret)
-        .WithRedirectUri("http://localhost:5000")
-        .WithAuthority(new Uri($"{configuration["AzureAd:Instance"]}{tenantId}"))
-        .Build();
+    // var app = ConfidentialClientApplicationBuilder.Create(clientId)
+    //     .WithClientSecret(clientSecret)
+    //     .WithRedirectUri(redirectUri)
+    //     .WithAuthority(new Uri($"{instance}{tenantId}"))
+    //     .Build();
 
-    var result = await app.AcquireTokenByAuthorizationCode(["https://graph.microsoft.com/Mail.Read"], code)
-        .ExecuteAsync();
+    // var result = await app.AcquireTokenByAuthorizationCode([scope], code)
+    //     .ExecuteAsync();
 
-    // Log the access token and refresh token
-    logger.LogInformation($"Access Token: {result.AccessToken}");
-    // logger.LogInformation($"Refresh Token: {result.RefreshToken}");
+    // log the access token. refresh token is not exposed
+    // logger.LogShInfo($"Access Token: {result.AccessToken}");
 
-    // use raw access token to create GraphServiceClient
+    // save access token to in memory cache
+    // memoryCache.Set("access_token", result.AccessToken, TimeSpan.FromMinutes(60));
+
+    // 3. use HttpClient to call raw OAuth2 token URL to retrieve access a refresh token
+    //   - this is not recommended due to security reasons
+    var httpClient = new HttpClient();
+    var tokenResponse = await httpClient.PostAsync(
+        $"{instance}/{tenantId}/oauth2/v2.0/token",
+        new FormUrlEncodedContent(new Dictionary<string, string>
+        {
+            { "client_id", clientId },
+            { "client_secret", clientSecret },
+            { "grant_type", "authorization_code" },
+            { "code", code },
+            { "redirect_uri", redirectUri },
+            { "scope", scope }
+        }));
+    var tokenContent = await tokenResponse.Content.ReadAsStringAsync();
+    logger.LogShInfo($"Token Response: {tokenContent}");
+
+    //  - parse the access token from the response
+    var token = JsonSerializer.Deserialize<Dictionary<string, object>>(tokenContent) ?? [];
+
+    // - log access and refresh tokens from the response
+    logger.LogShInfo($"Access Token: {token["access_token"]}");
+    logger.LogShInfo($"Refresh Token: {token["refresh_token"]}");
+
+    // parse unique_name from access_token
+    var accessToken = $"{token["access_token"]}";
+    var jwt = accessToken?.Split('.');
+    var jwtPayload = jwt?.Length > 1 ? JsonSerializer.Deserialize<Dictionary<string, object>>(Base64UrlEncoder.Decode(jwt[1])) : default;
+    var uniqueName = $"{jwtPayload?["unique_name"]}".ToLower();
+
+    logger.LogShInfo($"Unique Name: {uniqueName}");
+
+    // save access and refresh tokens to in memory cache
+    memoryCache.Set($"{uniqueName}_access_token", $"{token["access_token"]}", TimeSpan.FromMinutes(60));
+    memoryCache.Set($"{uniqueName}_refresh_token",$"{token["refresh_token"]}", TimeSpan.FromMinutes(60));
+
+    // save access and refresh tokens to scoped variables
+    // accessToken = $"{token["access_token"]}";
+    // refreshToken = $"{token["refresh_token"]}";
+
+    return Results.Ok(new
+    {
+        AccessToken = token["access_token"],
+        RefreshToken = token["refresh_token"]
+    });
+})
+.WithName("SaveToken")
+.WithDescription("Receives the authorization token, requests and stores the access and refresh tokens to cache")
+.WithDisplayName("Save Token")
+.WithTags("Token");
+
+app.MapGet("/api/attachments", async ([FromQuery][Required]string email, [FromServices]IMemoryCache memoryCache, [FromServices]ILogger<Program> logger) =>
+{
+    // 1. use locally logged on user credential (az login)
+    //   - user account must have tenant wide permissions to read email
+    // var credential = new DefaultAzureCredential();
+    
+    // 2. use client secret credential approach
+    //   - client must have tenant wide permission to read email
+    // var credential = new ClientSecretCredential(tenantId, clientId, clientSecret);
+
+    // 3. use username password credential approach (specify user single email and password)
+    //   - this doesn't work with federated accounts (PingID)
+    // var credential = new UsernamePasswordCredential(email, password, tenantId, clientId);
+    // var graphClient = new GraphServiceClient(credential);
+
+    // 4. retrieve access token from in memory cache
+    //   - list all items in memory cache
+    logger.LogShInfo($"Cache Items: {memoryCache.GetCurrentStatistics()?.CurrentEntryCount}");
+
+    // - retrieve access token from memory cache
+    email = email.ToLower();
+    var accessToken = memoryCache.Get($"{email}_access_token") as string;
+
+    // - use access token from scoped variable: accessToken
+    // accessToken = accessToken
+
+    if (string.IsNullOrEmpty(accessToken))
+    {
+        var message = $"Access token is missing for email: {email}";
+        logger.LogShError(message);
+        return Results.NotFound(message);
+    }
+    logger.LogShInfo($"Access Token: {accessToken}");
+
     var graphClient = new GraphServiceClient(
-        new BaseBearerTokenAuthenticationProvider(new TokenProvider { AccessToken = result.AccessToken }));
-
+        new BaseBearerTokenAuthenticationProvider(new TokenProvider { AccessToken = accessToken }));
+    
+    // use graph client to read email
     var userInbox = graphClient.Me;
 
-    var messagesResponse = await userInbox.Messages.GetAsync();
+    var messagesResponse = await userInbox.Messages.GetAsync(config => config.QueryParameters.Top = 50);
     if (messagesResponse == null || messagesResponse.Value == null)
     {
         return Results.NotFound();
@@ -220,21 +347,25 @@ app.MapGet("/", async (HttpContext context, IConfiguration configuration, ILogge
     var myMessage = messagesResponse.Value.FirstOrDefault(m => m.HasAttachments ?? false);
     if (myMessage == null) return Results.NotFound();
 
+
     var attachmentsResponse = await userInbox.Messages[myMessage.Id].Attachments.GetAsync();
-    var myAttachment = attachmentsResponse?.Value?.FirstOrDefault();
-    logger.LogInformation($"Attachment Count: {attachmentsResponse?.Value?.Count}, Attachment Name: {myAttachment?.Name}");
+    logger.LogShInfo($"Attachment Count: {attachmentsResponse?.Value?.Count}");
 
-    var attachmentTasks = messagesResponse.Value?
-        .Select(async m => (await userInbox.Messages[m.Id].Attachments.GetAsync())?.Value ?? []) ?? [];
+    foreach (var attachment in attachmentsResponse?.Value ?? [])
+    {
+        logger.LogShInfo($"Attachment Name: {attachment.Name}");
+    }
 
-    var attachments = (await Task.WhenAll(attachmentTasks))
-        .SelectMany(a => a)
+    var attachments = (attachmentsResponse?.Value ?? [])
         .Select(a => new { myMessage.Subject, AttachmentId = a.Id, Filename = a.Name });
 
     return attachments.Any() ? Results.Ok(attachments) : Results.NotFound();
     // return Results.File(fileAttachment.ContentBytes, fileAttachment.ContentType, fileAttachment.Name);
 })
-.WithName("GetAttachments");
+.WithName("GetAttachments")
+.WithDescription("Read email ttachments")
+.WithDisplayName("Get Attachments")
+.WithTags("Email");
 
 app.Run();
 
